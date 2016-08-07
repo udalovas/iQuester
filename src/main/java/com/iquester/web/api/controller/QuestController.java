e5a186739844080e9d5ed8f4970c06fe69169951
udalovas <al.udalec@gmail.com>
2016-08-07 19:39:48 +0100

    base path for repo

--- src/main/java/com/iquester/web/api/controller/QuestController.java
+++ src/main/java/com/iquester/web/api/controller/QuestController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/quests")
+@RequestMapping("/api/quests")
 public class QuestController {
 
     @Autowired
